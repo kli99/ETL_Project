917fb37ab72e2c1f9c183785229be85ab729c071
KEFAN LI <1350-kefan.li99@users.noreply.columbia.bootcampcontent.com>
2020-08-05 14:44:11 -0400

    New

--- Covid19 Cases Progression VS Mask Usage.sql
+++ Covid19 Cases Progression VS Mask Usage.sql
@@ -1,3 +1,22 @@
+-- Create Two Tables
+CREATE TABLE county (
+  COUNTYFP INT PRIMARY KEY,
+  County VARCHAR,
+  STATE VARCHAR,
+  CONFIRMED_CASES INT,
+  CONFIRMED_DEATHS INT
+);
+
+CREATE TABLE mask (
+  COUNTYFP INT PRIMARY KEY,
+  NEVER VARCHAR ,
+  RARELY VARCHAR,
+  SOMETIMES VARCHAR,
+  FREQUENTLY VARCHAR,
+  ALWAYS VARCHAR
+);
+
+
 -- Covid19 Cases Progression VS Mask Usage
 SELECT c.countyfp, c.county, c.state, c.confirmed_cases, c.confirmed_deaths, m. never, m.rarely, m.sometimes, m.frequently, m.always
 FROM county AS c
--- covidmask_db.sql
+++ covidmask_db.sql
@@ -1,4 +1,7 @@
-SELECT c.countyfp, c.county, c.state, c.confirmed_cases, c.confirmed_deaths, m.countyfp, m. never, m.rarely, m.sometimes, m.frequently, m.always
+-- Covid19 Cases Progression VS Mask Usage
+SELECT c.countyfp, c.county, c.state, c.confirmed_cases, c.confirmed_deaths, m. never, m.rarely, m.sometimes, m.frequently, m.always
 FROM county AS c
 INNER JOIN mask AS m ON
 m.countyfp=c.countyfp;
+
+
